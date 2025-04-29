405ce8c64b473eb4f89fd1aec71db2dedcf2acee
Sam Wolfson <swolfson2003@gmail.com>
2025-04-29 06:26:47 -0400

    update

--- ch9ex.hs
+++ ch9ex.hs
@@ -34,4 +34,30 @@ squishMap f [] = []
 squishMap f (x:xs) = (f x) ++ (squishMap f xs)
 
 squishAgain :: [[a]] -> [a]
-squishAgain (x:xs) = squishMap (x:) xs
+squishAgain xs = squishMap id xs 
+
+myMaximumBy :: (a->a->Ordering) -> [a] -> a
+myMaximumBy _ [x] = x
+myMaximumBy fcomp (x:xs) = 
+  case n of
+    LT -> y
+    _ -> x
+  where
+    y = myMaximumBy fcomp xs
+    n = fcomp x y
+
+myMinimumBy :: (a->a->Ordering) -> [a] -> a
+myMinimumBy _ [x] = x
+myMinimumBy fcomp (x:xs) = 
+  case n of
+    GT -> y
+    _ -> x
+  where
+    y = myMinimumBy fcomp xs
+    n = fcomp x y
+
+myMaximum :: (Ord a) => [a] ->a
+myMaximum x = myMaximumBy compare x
+
+myMinimum:: (Ord a) => [a] ->a
+myMinimum x = myMinimumBy compare x
